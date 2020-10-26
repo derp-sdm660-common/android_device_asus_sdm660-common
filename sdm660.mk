1d9bf2f81747073b152e24472f6e9b79767d2651
rahul9999xda <rahul9999xda@gmail.com>
2020-10-26 17:50:01 +0530

    sdm660-common: overlay: Change to Derp

--- sdm660.mk
+++ sdm660.mk
@@ -297,12 +297,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-derp
 
 # Permissions
 PRODUCT_COPY_FILES += \
