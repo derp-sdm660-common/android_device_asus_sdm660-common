7239ecd05a410ab3d738689d79e6a38f39872b8c
rahul9999xda <rahul9999xda@gmail.com>
2020-11-29 18:07:02 +0530

    Revert "sdm660-common: Update qcom sepolicy include"
    This reverts commit 521d0d07f2f70eef61a7a86f5ce189c82ebc713d.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,7 +160,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
